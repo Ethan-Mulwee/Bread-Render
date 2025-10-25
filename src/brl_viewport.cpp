263881368599e9714378b6bfbd3764a8093c9e8e
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-10-24 21:40:34 -0400

    remember to reset command buffer

--- src/brl_viewport.cpp
+++ src/brl_viewport.cpp
@@ -138,6 +138,8 @@ namespace brl {
             }
         }
 
+        resetRenderCommandBuffer(&viewport.renderCommandBuffer);
+
         // Draw Grid
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
